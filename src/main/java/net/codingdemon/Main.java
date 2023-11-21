e8b4305513976643efba2bd3ff24fdb0e3009b8c
Nitiraj Rathore <nitirar@amazon.com>
2023-11-21 15:06:43 +0530

    adding resource files using ant

--- src/main/java/net/codingdemon/Main.java
+++ src/main/java/net/codingdemon/Main.java
@@ -1,17 +1,12 @@
 package net.codingdemon;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
 
 public class Main {
     static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        Configurator.initialize(new DefaultConfiguration());
-        Configurator.setRootLevel(Level.INFO);
         logger.info("Hello World");
     }
 }
